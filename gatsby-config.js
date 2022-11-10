669e574ba3d6b2c8b1ec16b5842e9ecc27dffc65
fullstackdev <joujiyoshino.work@gmail.com>
2022-11-09 22:53:03 -0500

    init with new version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,31 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `my gatsby site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "04pQnDAIHxzLOLb8GeXlNA_Qx0HAHDkR9rW5UcwmBvM",
+        spaceId: "q9zkntem569v",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
